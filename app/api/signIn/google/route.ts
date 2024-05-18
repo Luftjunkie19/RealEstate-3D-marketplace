8fb99cae9f07430f7038af50f7493a7e4324a419
Luftjunkie <krawczyk0606@gmail.com>
2024-05-18 20:36:55 +0200

    signIn

--- app/api/signIn/google/route.ts
+++ app/api/signIn/google/route.ts
@@ -6,7 +6,7 @@ export async function POST (req:Request){
 
 
    const result = await supabase.auth.signInWithOAuth({provider:'google', options:{
-      redirectTo:'/'
+      redirectTo:'https://real-estate-3-d-marketplace.vercel.app'
    }});
 
    return Response.json(result);
