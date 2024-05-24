f7cdba88e3efa392ca0dd0c524c0baff70d30a56
Luftjunkie19 <kshjssjjs@gmail.com>
2024-05-24 11:33:49 +0200

    Implement invoking of special functions

--- app/api/getProperty/route.ts
+++ app/api/getProperty/route.ts
@@ -1,5 +1,30 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req:NextRequest){
-    const {country, propertyData}= await req.json();
+import axios from "axios";
+
+
+export async function POST(req:NextRequest, res:NextResponse){
+    const {address, city, state, zip}= await req.json();
+    try {
+        
+ const options= {url:"https://realty-mole-property-api.p.rapidapi.com/properties",method: 'GET',
+ headers: {
+    'X-RapidAPI-Key': process.env.NEXT_RAPIDAPI_KEY,
+    'X-RapidAPI-Host': process.env.NEXT_REALESTATEAPI_HOST
+  },
+  params:{
+    address,
+    city,
+    state,
+    zip
+ }};
+
+ const response = await axios.request(options);
+        const result = await response.data;
+        return NextResponse.json({result, error:null});
+    } catch (error) {
+        return NextResponse.json({result:null, error});
+        console.error(error);
+    }
 }
+
