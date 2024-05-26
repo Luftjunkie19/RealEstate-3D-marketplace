1a3a27eb8405bb21a58136bb18a66eddde32db08
Luftjunkie <krawczyk0606@gmail.com>
2024-05-26 23:24:51 +0200

    change redirections

--- app/api/signIn/github/route.ts
+++ app/api/signIn/github/route.ts
@@ -5,7 +5,7 @@ import { cookies } from "next/headers";
 export async function POST (req:Request){
 
    const result = await supabase.auth.signInWithOAuth({provider:'github', options:{
-  redirectTo:'http://localhost:3000'
+  redirectTo:'https://real-estate-3-d-marketplace.vercel.app'
    }});
 
   
