30c00b2f2bacdd88efb8d08f333a84a6c2fcccfe
Luftjunkie <krawczyk0606@gmail.com>
2024-05-28 23:31:21 +0200

    Add visual change to conference

--- app/components/Conference/ChatDrawer.tsx
+++ app/components/Conference/ChatDrawer.tsx
@@ -28,7 +28,7 @@ function ChatDrawer({shownChat, closeChat, }: Props) {
 
 
   return (
-    <div className={`duration-500 rounded-l-lg h-screen top-0 ${shownChat ? 'right-0 block' : '-right-full hidden'} bg-darkGray/95 max-w-xs w-full 2xl:hidden transition-all`}>
+    <div className={`sm:fixed lg:static duration-500 rounded-l-lg h-screen top-0 ${shownChat ? 'right-0 block' : '-right-full hidden'} bg-darkGray/95 max-w-xs w-full transition-all`}>
 <div className="w-full z-50 p-2 border-b-2 border-bgColor flex gap-6 justify-between">
     <p className='text-white flex gap-4 items-center'>Chat<FaRegMessage className="text-xl text-purple"/></p>
 <button onClick={closeChat}><IoCloseCircle className='text-red-500 text-3xl'/></button>
