c962d6f168b2ed3545700c6f46a9965d113bd061
Luftjunkie <krawczyk0606@gmail.com>
2024-05-30 22:21:27 +0200

    Fix Payform

--- app/components/list-estate/PayForm.tsx
+++ app/components/list-estate/PayForm.tsx
@@ -1,15 +1,26 @@
 'use client';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 type Props = {selectedOption:number | null}
 import { CreditCard, GooglePay } from 'react-square-web-payments-sdk';
 
 function PayForm({selectedOption}: Props) {
+  const [isMount, setIsMount] = useState<boolean>(false);
+
+  useEffect(() => {
+    /** Wait for 1 second for Square payment form initialisation */
+    const timer = setTimeout(() => setIsMount(true), 1000);
+    return () => {
+      clearTimeout(timer);
+      setIsMount(false);
+    };
+  }, []);
+
 
   return (
     <div className='max-w-md mx-auto m-0 flex flex-col gap-4 w-full p-2 rounded-lg bg-darkGray'>
       <p>In order to continue, you have to pay the fee.</p>
-      <GooglePay buttonColor="black"/>
-     <CreditCard>Pay {selectedOption !== null ? (20 + (selectedOption)).toFixed(2) : 20}$ fee</CreditCard>
+     {isMount && <GooglePay buttonColor="black"/>}
+      {isMount ? (<CreditCard>Pay {selectedOption !== null ? (20 + (selectedOption)).toFixed(2) : 20}$ fee</CreditCard>) : (<></>)}
     </div>
   )
 }
