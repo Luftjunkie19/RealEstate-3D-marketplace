07ccf3f8a764e11515667f104221294510b686e2
Luftjunkie <krawczyk0606@gmail.com>
2024-06-01 14:29:31 +0200

    Add notifications update

--- utils/supabase/getCurrentObject.ts
+++ utils/supabase/getCurrentObject.ts
@@ -2,8 +2,10 @@ import { supabase } from "./client";
 
 export const getCurrentObject=(tableName:string, filter:string, setObject:(obj:any)=>void)=>{
     const channel = supabase
-    .channel('room1').on('postgres_changes', {event:'*', schema:'public', filter},(payload)=>{
-        if(payload.eventType === 'UPDATE'){}
+    .channel('room1').on('postgres_changes', {event:'*', schema:'public', table:tableName, filter},(payload)=>{
+        if(payload.eventType === 'UPDATE'){
+            setObject(payload.new);
+        }
     }).subscribe();
 
     return ()=>{supabase.removeChannel(channel);}
