a44dd8ff0443828df0037a724436758fad432e60
Luftjunkie19 <kshjssjjs@gmail.com>
2024-06-01 14:48:53 +0200

    Change for the conference and presetup

--- app/components/Conference/ConferenceTiles/SharedScreenParticipantView.tsx
+++ app/components/Conference/ConferenceTiles/SharedScreenParticipantView.tsx
@@ -43,12 +43,12 @@ function SharedScreenParticipantView({participantId}: Props) {
 
   return (
     <>
-      <div className=" sm:max-w-40 3xl:max-w-60 w-full relative top-0 left-0 sm:max-h-32 h-fit">
+      <div className=" sm:max-w-40 3xl:max-w-60 w-full relative top-0 left-0 sm:max-h-32 ">
         {!micOn && <div className="absolute top-0 left-0 w-full h-full bg-darkGray/60 flex flex-col justify-center items-center">
           <FaMicrophoneSlash className='text-xl text-red-500'/>
           </div>}
-      {webcamOn ? <video width={'100%'} height={'100%'} autoPlay playsInline muted ref={webcamRef} controls={false} className={`w-full h-full rounded-lg border ${isActiveSpeaker ? 'border-green-400' : 'border-purple' }`}/> : 
-  <Image src={(participant.metaData as any).picture ?? DefaultImage} className="h-fit w-full object-cover rounded-lg" width={64} height={64} alt=""/>}
+      {webcamOn ? <video  width={'100%'} height={'100%'} autoPlay playsInline muted ref={webcamRef} controls={false} className={`w-full h-full rounded-lg border ${isActiveSpeaker ? 'border-green-400' : 'border-purple' }`}/> : 
+  <Image src={(participant.metaData as any).picture ? (participant.metaData as any).picture : DefaultImage} className="h-fit w-full object-cover rounded-lg" width={64} height={64} alt=""/>}
       <div className="absolute bottom-0 left-0 text-xs text-white font-semibold w-full h-6 rounded-b-lg bg-purple/40">
   <p className="p-1">{displayName}</p>
   </div>
