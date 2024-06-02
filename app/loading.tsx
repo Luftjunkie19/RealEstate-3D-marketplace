e96ca16218a8633126ac959eca22a3a75e25a56f
Luftjunkie19 <kshjssjjs@gmail.com>
2024-06-02 13:46:28 +0200

    add change

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,18 +1,15 @@
 import React from 'react'
-import {RotatingLines} from 'react-loader-spinner'
+import {InfinitySpin, RotatingLines} from 'react-loader-spinner'
 type Props = {}
 
 function Loading({}: Props) {
   return (
     <div className='w-full h-full flex flex-col gap-4 justify-center items-center'>
-      <RotatingLines
-  visible={true}
-  width='96'
-  strokeWidth="5"
-  animationDuration="0.75"
-  ariaLabel="rotating-lines-loading"
-  strokeColor='#703BF7'
+      <InfinitySpin
+  width="120"
+  color="#703BF7"
   />
+  <p className='text-white'>Loading...</p>
     </div>
   )
 }
