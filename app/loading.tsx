2c5657cabdb3e367c513fa64d8be5199abefb9ca
Luftjunkie <krawczyk0606@gmail.com>
2024-06-02 14:02:25 +0200

    remove loader

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,14 +1,9 @@
 import React from 'react'
-import {InfinitySpin, RotatingLines} from 'react-loader-spinner'
 type Props = {}
 
 function Loading({}: Props) {
   return (
     <div className='w-full h-full flex flex-col gap-4 justify-center items-center'>
-      <InfinitySpin
-  width="120"
-  color="#703BF7"
-  />
   <p className='text-white'>Loading...</p>
     </div>
   )
