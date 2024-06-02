e3160dd5fa2aa76ac83d3aff20a3afcc721367c5
Luftjunkie <krawczyk0606@gmail.com>
2024-06-02 14:12:40 +0200

    Add loading again

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,7 +4,7 @@ import {FadeLoader} from 'react-spinners';
 function Loading({}: Props) {
   return (
     <div className='w-full h-screen flex flex-col gap-4 justify-center items-center'>
-  <FadeLoader width={128} height={128} color='#703BF7'/>
+  <FadeLoader color='#703BF7'/>
   <p className='text-white'>Loading...</p>
     </div>
   )
