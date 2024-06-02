3f5b56da336032bbb97c48419cdc4f2334acd80e
Luftjunkie <krawczyk0606@gmail.com>
2024-06-02 13:41:54 +0200

    Download package and add loading lines

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
-
+import {RotatingLines} from 'react-loader-spinner'
 type Props = {}
 
 function Loading({}: Props) {
   return (
-    <div>Loading</div>
+    <div className='w-full h-full flex flex-col gap-4 justify-center items-center'>
+      <RotatingLines
+  visible={true}
+  width='96'
+  strokeWidth="5"
+  animationDuration="0.75"
+  ariaLabel="rotating-lines-loading"
+  strokeColor='#703BF7'
+  />
+    </div>
   )
 }
 
